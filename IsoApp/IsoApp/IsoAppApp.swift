533e97cc147cc8847cd6c09afa943667f2ad3933
sircrazypaper <127138479+sircrazypaper@users.noreply.github.com>
2025-03-02 08:15:57 +0100

    add files

---
+++ IsoApp/IsoApp/IsoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsoAppApp.swift
+//  IsoApp
+//
+//  Created by Jonathan Karniala Lehmann on 02/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct IsoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IsoApp/IsoAppTests/IsoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  IsoAppTests.swift
+//  IsoAppTests
+//
+//  Created by Jonathan Karniala Lehmann on 02/03/2025.
+//
+
+import Testing
+@testable import IsoApp
+
+struct IsoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
